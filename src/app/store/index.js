8b93fc4f5cdcd4afd22e1d321463ccb1430cd9f9
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-01 11:44:47 -0400

    Fix comma dangle issue

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -12,8 +12,8 @@ const store = createStore(
 	initialState,
 	compose(
 		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
-	)
+		window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+	),
 );
 
 export default store;
